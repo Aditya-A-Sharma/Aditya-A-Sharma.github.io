b3d994509edfb7973bcebd649cb9899177a4fb09
Aditya-A-Sharma <adityasharmadotin+github@gmail.com>
2023-09-28 22:44:07 +0530

    Updated

--- scriptsheet.js
+++ scriptsheet.js
@@ -1,20 +1,13 @@
+const themeToggleCheckbox = document.getElementById("theme-toggle-checkbox");
+
 function changeOnToggle() {
-
-  // Get references to the necessary elements
-  const themeToggleCheckbox = document.getElementById("theme-toggle-checkbox");
-
-  // Check if the checkbox is checked
   if (themeToggleCheckbox.checked) {
-    // Change the value of --lightColor to #222222
     document.documentElement.style.setProperty("--lightColor", "#222222");
     document.documentElement.style.setProperty("--darkColor", "#afafaf");
   } else {
-    // Reset the value of --lightColor to its original value (if needed)
     document.documentElement.style.setProperty("--lightColor", "#afafaf");
     document.documentElement.style.setProperty("--darkColor", "#222222");
   }
-  
 }
 
-// Add an event listener to the checkbox
 themeToggleCheckbox.addEventListener("change", changeOnToggle);
